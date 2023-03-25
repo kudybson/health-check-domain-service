f98c5603e2a3bbeb242b93a83cb8594ccee148a1
Krystian Kudyba <krystiankudyba111@gmail.com>
2023-03-25 17:54:05 +0100

    improved data extraction from jwt

--- domain-service-svc/src/main/java/pl/akh/domainservicesvc/controllers/Ping.java
+++ domain-service-svc/src/main/java/pl/akh/domainservicesvc/controllers/Ping.java
@@ -1,46 +1,43 @@
 package pl.akh.domainservicesvc.controllers;
 
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.security.auth.message.AuthException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.akh.domainservicesvc.utils.oauth.OAuthDataFacade;
+import pl.akh.domainservicesvc.utils.oauth.OAuthDataExtractorFacade;
 import pl.akh.domainservicesvc.utils.roles.HasAnyRole;
-import pl.akh.domainservicesvc.utils.roles.HasRolePatient;
 
 import java.security.Principal;
 
-//@RolesAllowed(value = "ROLE_ADMIN")
 @RestController
 @RequestMapping("/")
 public class Ping {
 
-    private final OAuthDataFacade oAuthDataFacade;
+    private final OAuthDataExtractorFacade oAuthDataExtractorFacade;
 
     @Autowired
-    public Ping(OAuthDataFacade oAuthDataFacade) {
-        this.oAuthDataFacade = oAuthDataFacade;
+    public Ping(OAuthDataExtractorFacade oAuthDataExtractorFacade) {
+        this.oAuthDataExtractorFacade = oAuthDataExtractorFacade;
     }
 
     @GetMapping("ping")
     @HasAnyRole
     public String pong(Principal principal) throws AuthException {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(oAuthDataFacade.getId());
+        stringBuilder.append(oAuthDataExtractorFacade.getId());
         stringBuilder.append("\n");
-        stringBuilder.append(oAuthDataFacade.getEmail());
+        stringBuilder.append(oAuthDataExtractorFacade.getUsername());
         stringBuilder.append("\n");
-        stringBuilder.append(oAuthDataFacade.getFullName());
+        stringBuilder.append(oAuthDataExtractorFacade.getEmail());
         stringBuilder.append("\n");
-        stringBuilder.append(oAuthDataFacade.getLastname());
+        stringBuilder.append(oAuthDataExtractorFacade.getFullName());
         stringBuilder.append("\n");
-        stringBuilder.append(oAuthDataFacade.getFirstName());
+        stringBuilder.append(oAuthDataExtractorFacade.getLastname());
         stringBuilder.append("\n");
-        stringBuilder.append(oAuthDataFacade.getRoles());
+        stringBuilder.append(oAuthDataExtractorFacade.getFirstName());
+        stringBuilder.append("\n");
+        stringBuilder.append(oAuthDataExtractorFacade.getRoles());
         stringBuilder.append("\n");
         return stringBuilder.toString();
     }
